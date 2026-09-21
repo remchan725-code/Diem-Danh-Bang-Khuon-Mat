5f50e146e8db4f9074b284be56efd25390029ec6
NamNguyen237 <NamNguyen@07kpq.onmicrosoft.com>
2026-09-21 23:16:39 +0700

    Update insightface_run.py
    đổi port để ko xung đột với service khác đã dùng port 8000

--- AI-Core/insightface_run.py
+++ AI-Core/insightface_run.py
@@ -111,7 +111,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "insightface_run:app",
         host="0.0.0.0",
-        port=8000,
+        port=8001,
         reload=True,
         app_dir=current_dir
     )
